95f62fb01427fe9643f6b09025f4bb14c12718a3
陳玨鄅 Jerry <jerryc@miniasp.com>
2020-05-27 18:06:48 +0800

    init

---
+++ EcpayPaymentGenerator/EcpayConfiguration.cs
@@ -0,0 +1,19 @@
+namespace EcpayPaymentGenerator.Configurations
+{
+    public class EcpayConfiguration
+    {
+        public EcpayServiceConfiguration Service { get; set; }
+        public EcpayPaymentConfiguration Payment { get; set; }
+    }
+
+    public class EcpayServiceConfiguration
+    {
+
+    }
+
+    public class EcpayPaymentConfiguration{
+
+    }
+
+
+}
