d617629759a19626ee2d0e986d13473d0d9ceefd
陳玨鄅 Jerry <jerryc@miniasp.com>
2020-06-08 12:53:16 +0800

    完成產生訂單後送出訂單的的單元測試。

--- FluentEcpay.Test/FluentEcpayTest_Payment.cs
+++ FluentEcpay.Test/FluentEcpayTest_Payment.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 using FluentAssertions;
 using FluentEcpay.Configurations;
 using FluentEcpay.Interfaces;
 using FluentEcpay.Models;
 using FluentEcpay.Enums;
+using System.Net.Http;
+using System.Net;
 
 namespace FluentEcpay.UnitTests
 {
     public class FluentEcpayTest_Payment
     {
         [Fact]
-        public void CreatePayment_InputIsConfiguration_ReturnPayment()
+        public void CreatePayment_InputServiceAndTransaction_ReturnPayment()
         {
             // Arrange
             var now = DateTime.Now;
@@ -29,6 +33,8 @@ namespace FluentEcpay.UnitTests
                 TotalAmount = 100,
                 PaymentType = "aio",
                 EncryptType = 1,
+                MerchantTradeNo = string.Empty, //dynamic
+                CheckMacValue = string.Empty,// dynamic
                 #region Optional
                 StoreID = null,
                 ItemURL = null,
@@ -120,6 +126,86 @@ namespace FluentEcpay.UnitTests
             actual.Should().BeEquivalentTo(expected, options => options
                  .Excluding(p => p.MerchantTradeNo)
                  .Excluding(p => p.CheckMacValue));
+        }
+
+        [Fact]
+        public async Task PaymentCheckout_InputPayment_ReturnHttpResponse()
+        {
+            // Arrange
+            var now = DateTime.Now;
+            #region Input
+            var service = new
+            {
+                Url = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",
+                MerchantId = "2000132",
+                HashKey = "5294y06JbISpM5x9",
+                HashIV = "v77hoKGq4kWxNNIS",
+                ServerUrl = "http://example.com",
+                ClientUrl = "https://tsem/payment/success"
+            };
+            var transaction = new
+            {
+                No = "tsem00003",
+                Description = "急診醫學會購物系統",
+                Date = now,
+                Method = PaymentMethod.Credit,
+                Items = new List<Item>{
+                    new Item{
+                        Name = "手機",
+                        Price=14000,
+                        Quantity = 2
+                    },
+                    new Item{
+                        Name = "隨身碟",
+                        Price = 900,
+                        Quantity = 10
+                    }
+                }
+            };
+            #endregion
+
+            IPayment payment = new PaymentConfiguration()
+                .Send.ToApi(
+                    url: service.Url)
+                .Send.ToMerchant(
+                    service.MerchantId)
+                .Send.UsingHash(
+                    key: service.HashKey,
+                    iv: service.HashIV)
+                .Return.ToServer(
+                    url: service.ServerUrl)
+                .Return.ToClient(
+                    url: service.ClientUrl)
+                .Transaction.New(
+                    no: transaction.No,
+                    description: transaction.Description,
+                    date: transaction.Date)
+                .Transaction.UseMethod(
+                    method: transaction.Method)
+                .Transaction.WithItems(
+                    items: transaction.Items)
+                .Generate();
+
+            var expected = HttpStatusCode.OK;
+
+            // Act
+            HttpResponseMessage actual;
+            var properties = typeof(IPayment).GetProperties();
+            var parameters = properties
+                .Where(property => property.Name != "URL")
+                .Where(property => property.GetValue(payment) != null)
+                .ToDictionary(property => property.Name, property => property.GetValue(payment).ToString());
+            using (var client = new HttpClient())
+            {
+                var content = new FormUrlEncodedContent(parameters);
+                var request = new HttpRequestMessage(HttpMethod.Post, payment.URL) { Content = content };
+                actual = await client.SendAsync(request);
+            }
+
+            // Assert
+            actual.StatusCode.Should().Be(expected);
+            var html = await actual.Content.ReadAsStringAsync();
+            html.Should().NotContainAny("交易失敗！ START", "常見失敗原因如下", "Transaction failed");
         }
     }
 }
